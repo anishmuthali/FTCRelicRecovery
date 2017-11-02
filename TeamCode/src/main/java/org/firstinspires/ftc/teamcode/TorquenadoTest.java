9c90283b370b07e8109d6682fcb00d546c15e8dd
Anish Muthali <anishmuthali@gmail.com>
2017-11-01 20:02:11 -0700

    Latest working code for moving arm up and down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TorquenadoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TorquenadoTest.java
@@ -24,24 +24,24 @@ public class TorquenadoTest extends OpMode{
         boolean on = gamepad1.left_bumper;
 
         // check if L2 is held down
-        boolean down = gamepad1.left_trigger != 0;
+        float triggerValue = gamepad1.left_trigger;
+        telemetry.addLine("left_trigger: " + gamepad1.left_trigger);
 
         // if L1 is held down, move the arm up
         if(on){
             telemetry.addLine("Arm moving up");
-            test.setPower(-0.5);
+            test.setPower(0.5);
         }
-
-        // if L1 is not held, keep the arm in place. provide enough power that the arm doesn't move up or down
-        else{
-            telemetry.addLine("Arm stationary");
-            test.setPower(-0.15);
-        }
-
         // if L2 is held, drop the arm down
-        if(down){
+        else if(triggerValue > 0.0){
             test.setPower(0);
         }
+        // if L1 is not held, keep the arm in place. provide enough power that the arm doesn't move up or down
+        else if(!on){
+            test.setPower(0.1);
+        }
+
+
 
         // TODO: create presets for different rows of the glyph tower (use dpad buttons)
     }
