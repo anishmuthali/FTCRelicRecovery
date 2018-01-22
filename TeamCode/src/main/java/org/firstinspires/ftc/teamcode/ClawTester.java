e3461c38cb0cadebec9171353645b1af0485aff1
Anish Muthali <anishmuthali@gmail.com>
2018-01-21 18:33:58 -0800

    Added code to test functionality of claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTester.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by anishmuthali on 1/21/18.
+ */
+@TeleOp(name="ClawTester")
+public class ClawTester extends OpMode {
+    private Servo leftClaw;
+    private Servo rightClaw;
+    @Override
+    public void init() {
+        leftClaw = hardwareMap.servo.get("left");
+        rightClaw = hardwareMap.servo.get("right");
+
+
+    }
+
+    @Override
+    public void loop() {
+
+
+        if(gamepad1.left_stick_x < 0){
+            rightClaw.setPosition(0);
+        }
+        else if(gamepad1.left_stick_x > 0){
+            rightClaw.setPosition(1);
+        }
+        else{
+            rightClaw.setPosition(0.5);
+        }
+
+        if(gamepad1.right_stick_x < 0){
+            leftClaw.setPosition(0);
+        }
+        else if(gamepad1.right_stick_x > 0){
+            leftClaw.setPosition(1);
+        }
+        else{
+            leftClaw.setPosition(0.5);
+        }
+    }
+}
